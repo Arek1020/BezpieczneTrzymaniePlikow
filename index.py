68a562350d91b5625f5a96a14c8f9767cd2a7b5c
tomek <tomek8855@gmail.com>
2023-07-01 08:44:58 +0200

    fix-decryption-file, store key

--- index.py
+++ index.py
@@ -1,70 +1,86 @@
 from flask import Flask, request, send_file
 from cryptography.fernet import Fernet
 import os
-
-import time
-
+import uuid
 import json
-f = open('config.json')
-config = json.load(f)
 
-print('KEY: ', config['key'])
 
 app = Flask(__name__)
-key = config['key']
-upload_folder = "uploaded_files/"
 encrypted_folder = "encrypted_files/"
 decrypted_folder = "decrypted_files/"
+key_path = "./key.txt"
 
+def write_filename_to_json(encrypted_filename, filename):
+    new_data = {
+        encrypted_filename : filename
+    }
+    with open('filename.json', 'r') as file:
+         data = json.load(file)
+    data.update(new_data)
+    with open("filename.json", "w") as write_file:
+         json.dump(data, write_file)
+
+def get_filename_from_json(file_code):
+    with open("filename.json", 'r') as file:
+        json_data = json.load(file)
+    if file_code in json_data:
+        return json_data[file_code]
+        
 def generate_key():
-    return Fernet.generate_key()
+    if not os.path.exists(key_path):
+        key = Fernet.generate_key()
+        with open(key_path, 'wb') as file_key:
+            file_key.write(key)
 
-def save_file(file, folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    filename = file.filename
-    filepath = os.path.join(folder, filename)
-    file.save(filepath)
-    return filename
+def get_key():
+    with open(key_path, 'rb') as file_key:
+        key = file_key.read()
+    return key
 
-def encrypt_file(filename):
+def encrypt_file(file,filename):
     if not os.path.exists(encrypted_folder):
         os.makedirs(encrypted_folder)
-    f = Fernet(key)
-    with open(os.path.join(upload_folder, filename), 'rb') as file:
-        data = file.read()
-        encrypted_data = f.encrypt(data)
-        time_stamp = str(time.time())
-        encrypted_filename = time_stamp + filename + '.enc'
-        with open(os.path.join(encrypted_folder, encrypted_filename), 'wb') as encrypted_file:
-            encrypted_file.write(encrypted_data)
-        return encrypted_filename
-
-def decrypt_file(filename):
+    key = get_key()
+    cipher = Fernet(key)
+    data = file.read()
+    encrypted_data = cipher.encrypt(data)
+    encrypted_filename = str(uuid.uuid4().hex) + ".enc"
+    write_filename_to_json(encrypted_filename, filename)
+    with open(os.path.join(encrypted_folder, encrypted_filename), 'wb') as encrypted_file:
+        encrypted_file.write(encrypted_data)
+    return encrypted_filename
+    
+def decrypt_file(file_code):
     if not os.path.exists(decrypted_folder):
         os.makedirs(decrypted_folder)
-    f = Fernet(key)
-    with open(os.path.join(encrypted_folder, filename), 'rb') as encrypted_file:
+    key = get_key()
+    cipher = Fernet(key)
+    with open(os.path.join(encrypted_folder, file_code), 'rb') as encrypted_file:
         encrypted_data = encrypted_file.read()
-        decrypted_data = f.decrypt(encrypted_data)
-        decrypted_filename = filename[:-4]
+        decrypted_data = cipher.decrypt(encrypted_data)
+        decrypted_filename = get_filename_from_json(file_code)
+        print(decrypted_filename)
         with open(os.path.join(decrypted_folder, decrypted_filename), 'wb') as decrypted_file:
             decrypted_file.write(decrypted_data)
         return decrypted_filename
 
 @app.route('/upload', methods=['POST'])
-def upload():
-    print(request.files)
-    uploaded_file = request.files['file']
-    filename = save_file(uploaded_file, upload_folder)
-    encrypted_filename = encrypt_file(filename)
+def upload_file():
+    file = request.files['file']
+    filename = file.filename
+    encrypted_filename = encrypt_file(file, filename)
     return f'File "{filename}" uploaded and encrypted as "{encrypted_filename}".'
-    return "test"
 
-@app.route('/download/<filename>', methods=['GET'])
-def download(filename):
-    decrypted_filename = decrypt_file(filename)
-    return send_file(os.path.join(decrypted_folder, decrypted_filename))
+@app.route('/download/<file_code>', methods=['GET'])
+def download(file_code):
+    decrypted_filename = decrypt_file(file_code)
+    return send_file(os.path.join(decrypted_folder, decrypted_filename), attachment_filename=decrypted_filename)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 if __name__ == '__main__':
     key = generate_key()
